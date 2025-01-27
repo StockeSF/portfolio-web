ebb10d8ab0c1bd4a6773eea88ef95baa40b040e8
Frank Contreras <fonseca.fr2709@gmail.com>
2025-01-26 23:41:55 -0400

    added 404 page

---
+++ src/i18n/404/config.ts
@@ -0,0 +1,13 @@
+import * as ESLANG from './es/lang.json'
+import * as ENLANG from './en/lang.json'
+
+export const labels = {
+  es: {
+    SEO: ESLANG.SEO,
+    CONT: ESLANG.CONTENT
+  },
+  en: {
+    SEO: ENLANG.SEO,
+    CONT: ENLANG.CONTENT
+  }
+}
