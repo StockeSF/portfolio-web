35a516cdfaf004f9e43b7eef3d52e65b71333cc5
Frank Contreras <fonseca.fr2709@gmail.com>
2025-02-03 18:48:53 -0400

    Add site url and redirects in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,25 +9,31 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://frankcff.netlify.app',
+
+  redirects: {
+    '/': '/es/'
+  },
+
   i18n: {
-      defaultLocale: 'es',
-      locales: ['es', 'en'],
-      routing: {
-          prefixDefaultLocale: true
-      }
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: true
+    }
 	},
 
   integrations: [
-      tailwind(),
-      icon({
-          iconDir: 'src/assets/icons/'
-      })
+    tailwind(),
+    icon({
+      iconDir: 'src/assets/icons/'
+    })
 	],
 
   experimental: {
-      svg: {
-          mode: 'sprite'
-      }
+    svg: {
+      mode: 'sprite'
+    }
 	},
 
   adapter: netlify()
