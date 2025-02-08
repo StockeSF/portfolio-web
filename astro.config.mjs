77939df5e3d4e46dbbe45e921efcc87290a5a506
Frank Fonseca <fonseca.fr2709@gmail.com>
2025-02-08 17:39:36 -0400

    remove trailing slash from redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   site: 'https://frankcff.netlify.app',
   trailingSlash: 'never',
   redirects: {
-    '/': '/es/',
+    '/': '/es',
   },
   i18n: {
     defaultLocale: 'es',
